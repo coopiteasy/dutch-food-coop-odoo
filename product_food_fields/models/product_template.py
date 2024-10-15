72a0f7a5071b10add766a4880caa1b14f51d4f08
Jur de Vries <jur.devries@simplicate.nl>
2024-10-15 14:45:59 +0200

    feat: GED-2: rename product expiry fields
    Renamed `days_until_expiry` to `use_by_days` and `days_until_bad_taste` to `best_before_days` for clarity.

--- product_food_fields/models/product_template.py
+++ product_food_fields/models/product_template.py
@@ -7,10 +7,8 @@ class ProductTemplate(models.Model):
     ingredients = fields.Text(help="Ingredients in the product")
     origin = fields.Char(help="Land where the product is produced, an ISO 3166 code")
     usage_tips = fields.Char(help="Usage tips")
-    # TODO: rename field
-    days_until_expiry = fields.Integer(help="Days until expiration")
-    # TODO: rename field
-    days_until_bad_taste = fields.Integer(help="Days until bad test")
+    use_by_days = fields.Integer(help="Days until use by date is reached")
+    best_before_days = fields.Integer(help="Days until best before date is reached")
     storage_temperature = fields.Integer(help="Storage temperature in Celcius")
     product_quality_id = fields.Many2one(
         "product_food_fields.product_quality", string="Product quality"
