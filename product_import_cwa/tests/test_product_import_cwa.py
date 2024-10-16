9cdd4b3d80f45acae95e193fd402178696772ad3
Jur de Vries <jur.devries@simplicate.nl>
2024-10-16 10:38:08 +0200

    feat: GED-12: add source_cwa_product_id to import change records
    Enhanced the import process by including the 'source_cwa_product_id' field in change records. This ensures that both the CWA product and the product template are correctly linked and written into a change record during imports. Added relevant tests to verify the new functionality.

--- product_import_cwa/tests/test_product_import_cwa.py
+++ product_import_cwa/tests/test_product_import_cwa.py
@@ -363,3 +363,31 @@ class TestProductImportCwa(TransactionCase):
         actual_result["affected_product_id"] = import_result.affected_product_id.id
 
         self.assertDictEqual(expected_result, actual_result)
+
+    def test_both_the_cwa_product_as_the_product_template_are_written_in_a_change_record(self):
+        cwa_product_obj = self.env["cwa.product"]
+        self.import_first_file(cwa_product_obj)
+        cwa_prod = cwa_product_obj.search([("omschrijving", "=", "BOEKWEIT")])
+        self.add_translations_for_brand_uom_cblcode_and_tax(cwa_prod)
+        cwa_prod.to_product()
+
+        imported_product = self.env["product.template"].search([("name", "=", "BOEKWEIT")])
+
+        self.import_second_file(cwa_product_obj)
+
+        expected_result = {
+            "affected_product_id": imported_product.id,
+            "source_cwa_product_id": cwa_prod.id
+        }
+
+        import_result = self.env['cwa.import.product.change'].search([('affected_product_id', "=", imported_product.id)])
+
+        actual_result = {
+            "affected_product_id": import_result.affected_product_id.id,
+            "source_cwa_product_id": import_result.source_cwa_product_id.id
+        }
+
+        self.assertDictEqual(expected_result, actual_result)
+
+
+        self.import_second_file(cwa_product_obj)
