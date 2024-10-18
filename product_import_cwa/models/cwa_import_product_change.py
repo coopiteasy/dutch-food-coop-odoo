cac96a6ef3511d59f400944cf6df6e6c98a20dd2
Jur de Vries <jur.devries@simplicate.nl>
2024-10-18 10:45:28 +0200

    feat: GED-12: fixed errors found by pre-commit

--- product_import_cwa/models/cwa_import_product_change.py
+++ product_import_cwa/models/cwa_import_product_change.py
@@ -1,74 +1,98 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 
-FIELDS_TO_COMPARE = (
-    "ingredients",
-)
+FIELDS_TO_COMPARE = ("ingredients",)
 
 
 class CwaImportProductChange(models.Model):
-    _name = 'cwa.import.product.change'
-    _description = 'CWA Import Product Change'
+    _name = "cwa.import.product.change"
+    _description = "CWA Import Product Change"
 
-    state = fields.Selection([
-        ('new', 'New'),
-        ('processed', 'Processed'),
-    ], string="State", default='new', required=True)
-    affected_product_id = fields.Many2one('product.template', string="Affected Product", required=True)
-    source_cwa_product_id = fields.Many2one('cwa.product', string="Source cwa product", required=True)
-    current_consumer_price = fields.Float(string="Current Consumer Price", required=True)
-    new_consumer_price = fields.Float(string="New Consumer Price", required=True)
-    product_supplierinfo_id = fields.Many2one('product.supplierinfo',
-                                              string="Product Supplierinfo",
-                                              compute='_compute_product_supplierinfo',
-                                              search='_compute_product_supplierinfo',
-                                              stored=False
-                                              )
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("processed", "Processed"),
+        ],
+        default="new",
+        required=True,
+    )
+    affected_product_id = fields.Many2one(
+        "product.template", string="Affected Product", required=True
+    )
+    source_cwa_product_id = fields.Many2one(
+        "cwa.product", string="Source cwa product", required=True
+    )
+    current_consumer_price = fields.Float(required=True)
+    new_consumer_price = fields.Float(required=True)
+    product_supplierinfo_id = fields.Many2one(
+        "product.supplierinfo",
+        compute="_compute_product_supplierinfo",
+        search="_search_product_supplierinfo",
+        stored=False,
+    )
 
-    changed_fields = fields.Char(string="Changed Fields", compute='_compute_changed_fields', store=False)
-    product_supplierinfo_ingredients = fields.Text(related="product_supplierinfo_id.ingredients")
-    affected_product_id_ingredients = fields.Text(related="affected_product_id.ingredients")
+    changed_fields = fields.Char(compute="_compute_changed_fields", store=False)
+    product_supplierinfo_ingredients = fields.Text(
+        related="product_supplierinfo_id.ingredients"
+    )
+    affected_product_id_ingredients = fields.Text(
+        related="affected_product_id.ingredients"
+    )
     affected_product_id_list_price = fields.Float(
         string="Current Price",
         compute="_compute_affected_product_id_list_price",
-        inverse="_set_affected_product_id_list_price",
-        store=True
+        inverse="_inverse_set_affected_product_id_list_price",
+        store=True,
     )
-    product_supplierinfo_list_price = fields.Float(related="product_supplierinfo_id.consumentenprijs")
-    product_supplierinfo_cost_price = fields.Float(related="product_supplierinfo_id.inkoopprijs")
-    product_supplierinfo_supplier = fields.Many2one(related="product_supplierinfo_id.partner_id")
+    product_supplierinfo_list_price = fields.Float(
+        related="product_supplierinfo_id.consumentenprijs"
+    )
+    product_supplierinfo_cost_price = fields.Float(
+        related="product_supplierinfo_id.inkoopprijs"
+    )
+    product_supplierinfo_supplier = fields.Many2one(
+        related="product_supplierinfo_id.partner_id"
+    )
 
-
-    @api.depends('affected_product_id.list_price')
+    @api.depends("affected_product_id.list_price")
     def _compute_affected_product_id_list_price(self):
         for record in self:
-            record.affected_product_id_list_price = record.affected_product_id.list_price
+            record.affected_product_id_list_price = (
+                record.affected_product_id.list_price
+            )
 
-    def _set_affected_product_id_list_price(self):
+    def _inverse_set_affected_product_id_list_price(self):
         for record in self:
             if record.affected_product_id:
-                record.affected_product_id.list_price = record.affected_product_id_list_price
+                record.affected_product_id.list_price = (
+                    record.affected_product_id_list_price
+                )
 
-    @api.depends('source_cwa_product_id')
-    def _compute_product_supplierinfo(self):
+    @api.depends("source_cwa_product_id")
+    def _search_product_supplierinfo(self):
         for record in self:
             if record.source_cwa_product_id:
-                # Replace 'unique_id' with the actual unique_id field of source_cwa_product_id if it's something else.
-                supplier_info = self.env['product.supplierinfo'].search(
-                    [('unique_id', '=', record.source_cwa_product_id.unique_id)], limit=1)
-                record.product_supplierinfo_id = supplier_info.id if supplier_info else False
+                supplier_info = self.env["product.supplierinfo"].search(
+                    [("unique_id", "=", record.source_cwa_product_id.unique_id)],
+                    limit=1,
+                )
+                record.product_supplierinfo_id = (
+                    supplier_info.id if supplier_info else False
+                )
             else:
                 record.product_supplierinfo_id = False
 
+    @api.depends("source_cwa_product_id")
+    def _compute_product_supplierinfo(self):
+        self._search_product_supplierinfo(self)
 
-    @api.depends('current_consumer_price', 'new_consumer_price')
+    @api.depends("current_consumer_price", "new_consumer_price")
     def _compute_changed_fields(self):
         for record in self:
-
             new_supplier_info = record.product_supplierinfo_id
             changed = []
 
             if record.current_consumer_price != record.new_consumer_price:
-                changed.append('list_price')
+                changed.append("list_price")
 
             if new_supplier_info:
                 for field in FIELDS_TO_COMPARE:
@@ -78,18 +102,20 @@ class CwaImportProductChange(models.Model):
                         if supplier_value != value:
                             changed.append(field)
 
-            record.changed_fields = ', '.join(changed)
+            record.changed_fields = ", ".join(changed)
 
     @api.model
     def open_form_view(self, record_id):
         if isinstance(record_id, list):
             record_id = record_id[0]  # Take the first id if a list is provided
         return {
-            'type': 'ir.actions.act_window',
-            'name': 'Open Form',
-            'view_mode': 'form',
-            'res_model': 'cwa.import.product.change',
-            'res_id': record_id,
-            'view_id': self.env.ref('product_import_cwa.view_cwa_import_product_change_form').id,
-            'target': 'new',
+            "type": "ir.actions.act_window",
+            "name": "Open Form",
+            "view_mode": "form",
+            "res_model": "cwa.import.product.change",
+            "res_id": record_id,
+            "view_id": self.env.ref(
+                "product_import_cwa.view_cwa_import_product_change_form"
+            ).id,
+            "target": "new",
         }
--- product_import_cwa/tests/test_product_import_cwa.py
+++ product_import_cwa/tests/test_product_import_cwa.py
@@ -295,18 +295,24 @@ class TestProductImportCwa(TransactionCase):
         supp_info1 = supplierinfo_obj.search([("product_name", "=", "BOEKWEIT")])
         self.assertEqual("INGREDIENTENN: BOEKWEIT, EEKHOORNS", supp_info1.ingredients)
 
-    def test_only_one_changed_record_is_created_when_an_imported_product_is_updated(self):
+    def test_only_one_changed_record_is_created_when_an_imported_product_is_updated(
+        self
+    ):
         cwa_product_obj = self.env["cwa.product"]
         self.import_first_file(cwa_product_obj)
         cwa_prod = cwa_product_obj.search([("omschrijving", "=", "BOEKWEIT")])
         self.add_translations_for_brand_uom_cblcode_and_tax(cwa_prod)
         cwa_prod.to_product()
 
-        imported_product = self.env["product.template"].search([("name", "=", "BOEKWEIT")])
+        imported_product = self.env["product.template"].search(
+            [("name", "=", "BOEKWEIT")]
+        )
 
         self.import_second_file(cwa_product_obj)
 
-        import_result = self.env['cwa.import.product.change'].search([('affected_product_id', "=", imported_product.id)])
+        import_result = self.env["cwa.import.product.change"].search(
+            [("affected_product_id", "=", imported_product.id)]
+        )
 
         self.assertEqual(len(import_result), 1)
 
@@ -317,11 +323,15 @@ class TestProductImportCwa(TransactionCase):
         self.add_translations_for_brand_uom_cblcode_and_tax(cwa_prod)
         cwa_prod.to_product()
 
-        imported_product = self.env["product.template"].search([("name", "=", "BOEKWEIT")])
+        imported_product = self.env["product.template"].search(
+            [("name", "=", "BOEKWEIT")]
+        )
 
         self.import_second_file(cwa_product_obj)
 
-        import_result = self.env['cwa.import.product.change'].search([('affected_product_id', "=", imported_product.id)])
+        import_result = self.env["cwa.import.product.change"].search(
+            [("affected_product_id", "=", imported_product.id)]
+        )
 
         expected_result = {
             "state": "new",
@@ -331,25 +341,33 @@ class TestProductImportCwa(TransactionCase):
         }
 
         actual_result = {
-            key: getattr(import_result, key, None) for key in expected_result.keys() if key != "affected_product_id"
+            key: getattr(import_result, key, None)
+            for key in expected_result.keys()
+            if key != "affected_product_id"
         }
         actual_result["affected_product_id"] = import_result.affected_product_id.id
 
         self.assertDictEqual(expected_result, actual_result)
 
-    def test_a_changed_record_is_changed_when_an_imported_product_is_updated_again(self):
+    def test_a_changed_record_is_changed_when_an_imported_product_is_updated_again(
+        self
+    ):
         cwa_product_obj = self.env["cwa.product"]
         self.import_first_file(cwa_product_obj)
         cwa_prod = cwa_product_obj.search([("omschrijving", "=", "BOEKWEIT")])
         self.add_translations_for_brand_uom_cblcode_and_tax(cwa_prod)
         cwa_prod.to_product()
 
-        imported_product = self.env["product.template"].search([("name", "=", "BOEKWEIT")])
+        imported_product = self.env["product.template"].search(
+            [("name", "=", "BOEKWEIT")]
+        )
 
         self.import_second_file(cwa_product_obj)
         self.import_third_file(cwa_product_obj)
 
-        import_result = self.env['cwa.import.product.change'].search([('affected_product_id', "=", imported_product.id)])
+        import_result = self.env["cwa.import.product.change"].search(
+            [("affected_product_id", "=", imported_product.id)]
+        )
 
         expected_result = {
             "state": "new",
@@ -359,37 +377,42 @@ class TestProductImportCwa(TransactionCase):
         }
 
         actual_result = {
-            key: getattr(import_result, key, None) for key in expected_result.keys() if key != "affected_product_id"
+            key: getattr(import_result, key, None)
+            for key in expected_result.keys()
+            if key != "affected_product_id"
         }
         actual_result["affected_product_id"] = import_result.affected_product_id.id
 
         self.assertDictEqual(expected_result, actual_result)
 
-    def test_both_the_cwa_product_as_the_product_template_are_written_in_a_change_record(self):
+    def test_cwa_product_and_the_product_template_are_written(self):
         cwa_product_obj = self.env["cwa.product"]
         self.import_first_file(cwa_product_obj)
         cwa_prod = cwa_product_obj.search([("omschrijving", "=", "BOEKWEIT")])
         self.add_translations_for_brand_uom_cblcode_and_tax(cwa_prod)
         cwa_prod.to_product()
 
-        imported_product = self.env["product.template"].search([("name", "=", "BOEKWEIT")])
+        imported_product = self.env["product.template"].search(
+            [("name", "=", "BOEKWEIT")]
+        )
 
         self.import_second_file(cwa_product_obj)
 
         expected_result = {
             "affected_product_id": imported_product.id,
-            "source_cwa_product_id": cwa_prod.id
+            "source_cwa_product_id": cwa_prod.id,
         }
 
-        import_result = self.env['cwa.import.product.change'].search([('affected_product_id', "=", imported_product.id)])
+        import_result = self.env["cwa.import.product.change"].search(
+            [("affected_product_id", "=", imported_product.id)]
+        )
 
         actual_result = {
             "affected_product_id": import_result.affected_product_id.id,
-            "source_cwa_product_id": import_result.source_cwa_product_id.id
+            "source_cwa_product_id": import_result.source_cwa_product_id.id,
         }
 
         self.assertDictEqual(expected_result, actual_result)
-
 
         self.import_second_file(cwa_product_obj)
 
@@ -400,11 +423,15 @@ class TestProductImportCwa(TransactionCase):
         self.add_translations_for_brand_uom_cblcode_and_tax(cwa_prod)
         cwa_prod.to_product()
 
-        imported_product = self.env["product.template"].search([("name", "=", "BOEKWEIT")])
+        imported_product = self.env["product.template"].search(
+            [("name", "=", "BOEKWEIT")]
+        )
 
         self.import_second_file(cwa_product_obj)
 
-        import_result = self.env['cwa.import.product.change'].search([('affected_product_id', "=", imported_product.id)])
+        import_result = self.env["cwa.import.product.change"].search(
+            [("affected_product_id", "=", imported_product.id)]
+        )
 
         changed_fields = "list_price, ingredients"
 
